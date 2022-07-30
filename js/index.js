ea199ae9e1d3dac79435d4d119f8d18223831fac
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-07-30 16:44:28 +0200

    added html and css

---
+++ js/index.js
@@ -0,0 +1,24 @@
+const splashScreen = document.querySelector('#splash-screen');
+const gameBoard = document.querySelector('#game-board');
+
+window.onload = () => {
+    document.getElementById('start-button').onclick = () => {
+        gameArea.start();
+    }
+}
+
+const gameArea = {
+    canvas: document.createElement('canvas'),
+    frame: 0,
+    start: function(){
+        splashScreen.style.display = 'none';
+        this.canvas.width = 1000;
+        this.canvas.height = 500;
+        this.context = this.canvas.getContext('2d'); 
+        gameBoard.appendChild(this.canvas); // appending this.canvas to gameBoard
+        this.interval = setInterval(update, 20); //update game area every 20ms 
+    },
+    stop: function(){
+        clearInterval(this.interval);
+    }
+}
