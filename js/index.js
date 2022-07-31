5915ef378bf7eb1bacdea16fb48e69b4bdfc3f63
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-07-31 12:13:37 +0200

    added background

--- js/index.js
+++ js/index.js
@@ -9,6 +9,9 @@ window.onload = () => {
 const splashScreen = document.querySelector('#splash-screen');
 const gameBoard = document.querySelector('#game-board');
 
+//Images 
+const background = new Image();
+background.src ='/assets/images/background.jpg';
 
 //gameArea Object
 const gameArea = {
@@ -16,13 +19,20 @@ const gameArea = {
     frame: 0,
     start: function(){
         splashScreen.style.display = 'none';
+        gameBoard.style.display = 'flex';
         this.canvas.width = 1000;
         this.canvas.height = 500;
         this.context = this.canvas.getContext('2d'); 
         gameBoard.appendChild(this.canvas); // appending this.canvas to gameBoard
         this.interval = setInterval(update, 20); //update game area every 20ms 
+        this.context.drawImage(background, 0, 0, this.canvas.width, this.canvas.height);
     },
     stop: function(){
         clearInterval(this.interval);
     }
+}
+
+//update function
+function update(){
+    
 }
